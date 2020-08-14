b5ac880d8a46993caa1e9c35085f535bd00fddd1
Your Name <isaac.diaz@unitec.edu>
2020-08-14 15:11:38 -0600

    metodo añadir

---
+++ Lab4_IsaacDiazDavidZavala/src/lab4_isaacdiazdavidzavala/MetodosI.java
@@ -0,0 +1,49 @@
+package lab4_isaacdiazdavidzavala;
+
+import java.util.Scanner;
+
+public class MetodosI {
+
+    static Scanner leer = new Scanner(System.in);
+
+    public Criminal añadirCriminal() {
+        String nombre;
+        int edad;
+        String genero;             //solo puede ser 1. Masculino o 2.Femenino
+        String pais;
+        boolean encarcelado;       //0: no está encarcelado    1: está topadito
+        String desc;
+        System.out.print("Ingrese nombre del criminal: ");
+        nombre = leer.next();
+        System.out.print("Edad: ");
+        edad = leer.nextInt();
+        System.out.print("Género:    1. Hombre  2. Mujer");
+        int o = leer.nextInt();
+        while (o!=1 || o!=2) {
+            o = leer.nextInt();
+        }
+        if(o==1){
+            genero = "Hombre";
+        } else{
+            genero = "Mujer";
+        }
+        System.out.print("País: ");
+        pais = leer.next();
+        System.out.println("¿Está encarcelado?:    0. No  1. Sí");
+        o = leer.nextInt();
+        while (o!=0 || o!=1) {
+            o = leer.nextInt();
+        }
+        if(o==0){
+            encarcelado = false;
+        } else{
+            encarcelado = true;
+        }
+        System.out.print("Descripción del criminal: ");
+        desc = leer.next();
+        
+        Criminal c = new Criminal(nombre, edad, genero, pais, encarcelado, desc);
+        
+        return c;
+    }
+}
