af06f9cf5b2bbd03de0d91e3145033e820cc2bec
Your Name <isaac.diaz@unitec.edu>
2020-08-14 18:44:24 -0600

    Trabajando en mods

--- Lab4_IsaacDiazDavidZavala/src/lab4_isaacdiazdavidzavala/MetodosI.java
+++ Lab4_IsaacDiazDavidZavala/src/lab4_isaacdiazdavidzavala/MetodosI.java
@@ -19,31 +19,45 @@ public class MetodosI {
         edad = leer.nextInt();
         System.out.print("Género:    1. Hombre  2. Mujer");
         int o = leer.nextInt();
-        while (o!=1 || o!=2) {
+        while (o != 1 || o != 2) {
             o = leer.nextInt();
         }
-        if(o==1){
+        if (o == 1) {
             genero = "Hombre";
-        } else{
+        } else {
             genero = "Mujer";
         }
         System.out.print("País: ");
         pais = leer.next();
         System.out.println("¿Está encarcelado?:    0. No  1. Sí");
         o = leer.nextInt();
-        while (o!=0 || o!=1) {
+        while (o != 0 || o != 1) {
             o = leer.nextInt();
         }
-        if(o==0){
+        if (o == 0) {
             encarcelado = false;
-        } else{
+        } else {
             encarcelado = true;
         }
-        System.out.print("Descripción del criminal: ");
-        desc = leer.next();
-        
+        System.out.print("Descripción del criminal (una oración): ");
+        desc = leer.nextLine();
+        desc = leer.nextLine();
+
         Criminal c = new Criminal(nombre, edad, genero, pais, encarcelado, desc);
-        
+
         return c;
+    }
+
+    public void modCriminal() {
+        System.out.print("¿Qué vas a modificar?");
+        System.out.println("1. Criminal     2. Terrorista       3. Asesino      4. Secuestrador");
+        int o = leer.nextInt();
+        while ((o >= 1) == false && (o <= 4) == false) {
+            o = leer.nextInt();
+        }
+
+        if (o == 1) {       //MODIFICAR CRIMINAL
+            //se llevó al main
+        }
     }
 }
